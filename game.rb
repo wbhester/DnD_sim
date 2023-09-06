6f0688c72d9f533b4461ff7808f096ae55b895e5
wbhester <hester6336@gmail.com>
2023-09-05 22:00:27 -0500

    Trying to print job title not working yet

--- game.rb
+++ game.rb
@@ -1,6 +1,7 @@
 require_relative 'thief'
 require_relative 'die_d6'
-player1 = Thief.new
+player1 = Thief.new('Mason')
 d6 = D6Dice.new
 puts d6.roll
 puts "Theif hp: #{player1.hp}"
+puts player1.job
