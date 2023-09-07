b54632229e5cab87e59c7105890ebd7d83fd0c9c
wbhester <hester6336@gmail.com>
2023-09-06 20:58:29 -0500

    added name

--- game.rb
+++ game.rb
@@ -1,7 +1,7 @@
 require_relative 'thief'
 require_relative 'die_d6'
-player1 = Thief.new('Mason')
+player1 = Thief.new('Dude', 'Mason')
 d6 = D6Dice.new
 puts d6.roll
 puts "Theif hp: #{player1.hp}"
-puts player1.job
+puts "#{player1.name}: #{player1.job}"
